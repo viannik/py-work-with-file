c36fcaaeb7c2d517ecb26ddeb7b2b51ab40b9fb4
viannik <ivan.nikitski@gmail.com>
2025-07-03 10:22:51 +0300

    add report generation function to process data and output results

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+def create_report(
+    data_file_name: str,
+    report_file_name: str,
+) -> None:
+    report = {
+        "supply": 0,
+        "buy": 0,
+    }
+    for line in open(data_file_name):
+        line = line.split(",")
+        operation_type = line[0]
+        amount = int(line[1][:-1])
+        report[operation_type] += amount
+    report["result"] = report["supply"] - report["buy"]
+
+    open(report_file_name, "w").write(
+        f"supply,{report["supply"]}\n"
+        f"buy,{report["buy"]}\n"
+        f"result,{report["result"]}\n"
+    )
